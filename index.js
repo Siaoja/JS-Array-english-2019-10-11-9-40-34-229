205d336280cdfcb5902ea7c7d829a0af8d5afcf7
Siaoja <1610692147@qq.com>
2020-06-12 21:36:39 +0800

    complete requirement

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(a instanceof Array);//false
+console.log(b instanceof Array);//true
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+for(let i =0; i < a.length; i++){
+    a[i] = a[i] * 2;
+}
+console.log(a);
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +19,50 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+for(let i = 1; i < 4; i++){
+    switch(i){
+        case 1:
+            console.log(colors.join(" "));
+            break;
+        case 2:
+            console.log(colors.join("+"));
+            break;
+        case 3:
+            console.log(colors.join(","));
+            break;
+    }
+}
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a.sort(function(a,b){ return b-a; });
+console.log(a);
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var map = new Map();
+for (let i = 0; i < a.length; i++) {
+    if (map.has(a[i])) {
+        map.set(a[i], map.get(a[i]) + 1);
+    } else {
+        map.set(a[i], 1);
+    }
+}
+var max = 0;
+var maxchar = [];
+map.forEach(function (value, key) {
+    if (max < value) {
+        max = value;
+        maxchar=[];
+        maxchar.push(key);
+    }else if(max == value){
+        maxchar.push(key);
+    }
+});
+for(let i = 0; i < maxchar.length; i++){
+    console.log(maxchar[i]);
+}
